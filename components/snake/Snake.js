a7b00db157b3f0fa02272fa3de70cb694431e4a0
LiuQBinh <luuquocbinh@gmail.com>
2020-04-08 19:00:08 +0700

    adjust GUI

--- components/snake/Snake.js
+++ components/snake/Snake.js
@@ -6,10 +6,10 @@ export default (props) => {
       {props.snakeDots.map((dot, i) => {
         const style = {
           left: `${dot[0]}%`,
-          top: `${dot[1]}%`
+          top: `${dot[1]}%`,
         }
         return (
-          <div className="snake-dot" key={i} style={style}></div>
+          <div className={`snake-dot ${i + 1 === props.snakeDots.length ? 'head' : ''}`} key={i} style={style}></div>
         )
       })}
     </div>
